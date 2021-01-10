f44417d0d498dad0e5b158fa6aefc4a250f6e48c
Flinton <shifenteng76@gmail.com>
2021-01-10 16:08:53 +0800

    从服务器获取数据(运行成功2020/1/10)

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,36 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-import { HeroService } from '../hero.service';
 
 import { Hero } from '../hero';
-
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: [ './hero-detail.component.css' ]
 })
 export class HeroDetailComponent implements OnInit {
-  hero: Hero | any;
+  hero: Hero | any; 
 
   constructor(
     private route: ActivatedRoute,
     private heroService: HeroService,
     private location: Location
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.getHero();
   }
 
   getHero(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+    const id = Number(this.route.snapshot.paramMap.get('id'))
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
   }
 
   goBack(): void {
     this.location.back();
+  }
+
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
   }
 }
--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -5,7 +5,6 @@ import { Hero } from './hero';
 @Injectable({
   providedIn: 'root',
 })
-
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
@@ -20,7 +19,7 @@ export class InMemoryDataService implements InMemoryDbService {
       { id: 19, name: 'Magma' },
       { id: 20, name: 'Tornado' }
     ];
-    return { heroes };
+    return {heroes};
   }
 
   // Overrides the genId method to ensure that a hero always has an id.
